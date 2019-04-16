3813c30fd7fdb19da66d159cf140d659d89acd7d
rachangowda <rachanr7@gmial.com>
2019-04-16 15:01:51 +0530

    Collapsing toolbar and tablayout tabs added

---
+++ app/src/main/java/com/test/gambit/GamesFragment.java
@@ -0,0 +1,25 @@
+package com.test.gambit;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GamesFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_games, container, false);
+        return view;
+    }
+
+}
--- app/src/main/java/com/test/gambit/MainActivity.java
+++ app/src/main/java/com/test/gambit/MainActivity.java
@@ -1,13 +1,36 @@
 package com.test.gambit;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.sliding_tabs)
+    TabLayout tabLayout;
+
+    @BindView(R.id.viewpager)
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Butterknife to the view
+        ButterKnife.bind(this);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(this, getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        pager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        tabLayout.setupWithViewPager(pager);
     }
 }
